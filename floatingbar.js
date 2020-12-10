db2f9fb67123738f5fff208d96e7423a9971cee7
azhimeblog <azhime.blog@gmail.com>
2020-12-11 00:11:56 +0700

    update floating bar

---
+++ floatingbar.js
@@ -0,0 +1,3 @@
+function clickcloseline() {
+    document.getElementById("topbar").innerHTML = "  <div class='row floatbardesign'><h2 class='floatbartext'>Add Friend</h2><a class='imgflot' target='_blank' href='https://lin.ee/a8abhjq'><img src='/asset/M.png'></a><span class='floatbartext'>LINE ID : @feaw88</span><p class='floatbardark' onclick='clickcloseline()'>เปิดหน้าต่าง</p></div> ";
+}
